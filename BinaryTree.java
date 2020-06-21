d10ae56db33b3ec09700dbbdb619e45109d7722d
Dwqe <45004307+ZPyhagoras@users.noreply.github.com>
2020-06-21 20:53:21 +0800

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+    TreeNode root;
+
+    BinaryTree() {root = null;}
+
+    BinaryTree(int[] nodeVals) {
+        root = new TreeNode(nodeVals[0]);
+        Queue<TreeNode> qNodes = new LinkedList<>();
+        qNodes.add(root);
+        int i = 1;
+        while (!qNodes.isEmpty()) {
+            TreeNode fNode = qNodes.poll();
+
+            int lVal = Integer.MIN_VALUE, rVal = Integer.MIN_VALUE;
+
+            if (i < nodeVals.length) {
+                lVal = nodeVals[i];  i++;
+            }
+            if (lVal != Integer.MIN_VALUE) {
+                fNode.left = new TreeNode(lVal);
+                qNodes.add(fNode.left);
+            }
+
+            if (i < nodeVals.length) {
+                rVal = nodeVals[i];  i++;
+            }
+            if (rVal != Integer.MIN_VALUE) {
+                fNode.right = new TreeNode(rVal);
+                qNodes.add(fNode.right);
+            }
+        }
+    }
+
+    void prettyPrintTree(TreeNode node, String prefix, boolean isLeft) {
+        if (node == null) {
+            System.out.println("Empty tree");
+            return;
+        }
+    
+        if (node.right != null) {
+            prettyPrintTree(node.right, prefix + (isLeft ? "│   " : "    "), false);
+        }
+    
+        System.out.println(prefix + (isLeft ? "└── " : "┌── ") + node.val);
+    
+        if (node.left != null) {
+            prettyPrintTree(node.left, prefix + (isLeft ? "    " : "│   "), true);
+        }
+    }
+
+    void prettyPrintTree() {
+        prettyPrintTree(root,  "", true);
+    }
+}
