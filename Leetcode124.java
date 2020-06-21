e074753344ecae8ec8bd8dcaaf9bbc55470bf2db
Dwqe <45004307+ZPyhagoras@users.noreply.github.com>
2020-06-21 20:53:23 +0800

    Create Leetcode124.java

---
+++ Leetcode124.java
@@ -0,0 +1,19 @@
+public class Leetcode124 {
+    private int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxNode(root);
+        return ans;
+    }
+
+    private int maxNode(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxLeft = Math.max(maxNode(root.left), 0);
+        int maxRight = Math.max(maxNode(root.right), 0);
+
+        ans = Math.max(ans, root.val + maxLeft + maxRight);
+
+        return Math.max(maxLeft, maxRight) + root.val;
+    }
+}
