7b3dea271be0534c24f0d9442685c852ab7881a0
Dwqe <45004307+ZPyhagoras@users.noreply.github.com>
2020-06-23 10:49:17 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Leetcode124 Solution124 = new Leetcode124();
+        // test124();
+        Leetcode67 Solution = new Leetcode67();
+        System.out.println(Solution.addBinary("1", "11110"));
+    }
+
+    private static void test124() {
         int iMin = Integer.MIN_VALUE;
         int[] vals = {-10, 9, 20, iMin, 999};
         BinaryTree newTree = new BinaryTree(vals);
         newTree.prettyPrintTree();
-        /*
+        
         TreeNode TreeRoot = newTree.root;
         Leetcode124 Solution = new Leetcode124();
         System.out.println(Solution.maxPathSum(TreeRoot));
-        */
     }
 }
