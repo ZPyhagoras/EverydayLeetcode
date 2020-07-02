0940d31934a892e6b7a2691ffba477840614844e
ZPythagoras <zqydwqe@gmail.com>
2020-07-02 16:38:56 +0800

    Modified Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // test124();
-        Leetcode67 Solution = new Leetcode67();
-        System.out.println(Solution.addBinary("1", "11110"));
+        Leetcode378 Solution = new Leetcode378();
+        int[][] matrix = new int[][]{{1,5,9},{10,11,13},{12,13,15}};
+        int ans = Solution.kthSmallest(matrix, 8);
+        System.out.println(ans);
     }
     /*
     private static void test124() {
