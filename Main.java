cf5b9ff161b815a177d00497957a43af835dd4a8
ZPythagoras <zqydwqe@gmail.com>
2020-07-03 23:12:24 +0800

    Modified Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Leetcode378 Solution = new Leetcode378();
-        int[][] matrix = new int[][]{{1,5,9},{10,11,13},{12,13,15}};
-        int ans = Solution.kthSmallest(matrix, 8);
-        System.out.println(ans);
+
     }
     /*
     private static void test124() {
