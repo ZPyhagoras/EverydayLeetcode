edce0a988fc475a903441d5b7c6eec97ab25aa4e
ZPythagoras <zqydwqe@gmail.com>
2020-07-04 11:57:21 +0800

    Add solution

--- Leetcode32.java
+++ Leetcode32.java
@@ -25,4 +25,39 @@ public class Leetcode32 {
         }
         return ans;
     }
+
+    private int twiceTraverseSolution(String s) {
+        int left, right, ans;
+        ans = left = right = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left == right) {
+                ans = Math.max(ans, left + right);
+            }
+            else if (left < right) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left == right) {
+                ans = Math.max(ans, left + right);
+            }
+            else if (right < left) {
+                left = right = 0;
+            }
+        }
+        return ans;
+    }
 }
