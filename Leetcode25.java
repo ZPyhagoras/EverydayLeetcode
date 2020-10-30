6decb3931cc3bbc667dcc24253985924f0330073
ZPythagoras <zqydwqe@gmail.com>
2020-10-30 17:05:19 +0800

    25

--- Leetcode25.java
+++ Leetcode25.java
@@ -1,7 +1,44 @@
+import java.util.List;
+
 import Datastructure.ListNode;
 
 public class Leetcode25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+
+        while (head != null) {
+            ListNode tail = pre;
+            int cnt = 0;
+            while (cnt < k && tail != null) {
+                tail = tail.next;
+                cnt++;
+            }
+            if (tail == null) {
+                return dummyHead.next;
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = reverseNodes(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return dummyHead.next;
+    }
+
+    private ListNode[] reverseNodes(ListNode head, ListNode tail) {
+        ListNode end = tail.next, p = head;
+        while (end != tail) {
+            ListNode nxt = p.next;
+            p.next = end;
+            end = p;
+            p = nxt;
+        }
+        return new ListNode[]{tail, head};
     }
 }
